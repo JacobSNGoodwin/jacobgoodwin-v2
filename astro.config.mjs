80431e4231fa2f05832e92f8d9deacc2aec070fd
Jacob Goodwin <jacob.goodwin@gmail.com>
2022-06-11 12:32:15 -0700

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+import solid from "@astrojs/solid-js";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), vue(), solid()]
+});
