f7202cd4d1476df8c67489effcf2f6a5fc7fc180
Jacob Goodwin <jacob.goodwin@gmail.com>
2022-06-11 20:24:09 -0700

    many font and style updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
-import vue from "@astrojs/vue";
-import solid from "@astrojs/solid-js";
+import react from '@astrojs/react';
+import vue from '@astrojs/vue';
+import solid from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), solid()]
+  integrations: [react(), vue(), solid()],
+  site: 'https://jacobgoodiwn.me',
+  trailingSlash: 'never',
+  server: {
+    host: true,
+  },
 });
