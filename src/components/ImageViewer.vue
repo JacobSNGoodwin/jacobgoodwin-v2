4d3d3d8d92f772f81801f8cfd17e30810d8e1a6c
Jacob Goodwin <jacob.goodwin@gmail.com>
2022-06-18 16:49:05 -0700

    create a full screen component in vue

---
+++ src/components/ImageViewer.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useFullscreen } from '@vueuse/core';
+
+const containerEl = ref<HTMLElement | null>(null);
+const { isFullscreen, toggle } = useFullscreen(containerEl);
+</script>
+
+<template>
+  <!-- Add a preview container visible if !fullScreen -->
+  <div class="fs-container" ref="containerEl">
+    <div class="fs-content" v-if="isFullscreen">
+      <h3>Fullscreen content</h3>
+    </div>
+  </div>
+  <button @click="toggle">Open Full screen</button>
+</template>
+
+<style scoped lang="css">
+/* .fs-container {
+  &:fullscreen {
+    color: white;
+  }
+} */
+.fs-content {
+  max-width: 2400px;
+  height: 100vh;
+  background-color: white;
+  margin: auto;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
