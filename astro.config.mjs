2565346becda6670451dc33e85f005b4afaf68f6
Jacob Goodwin <jacob.goodwin@gmail.com>
2022-10-14 12:56:46 -0700

    add astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import vue from '@astrojs/vue';
 import solid from '@astrojs/solid-js';
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), solid()],
-  site: 'https://jacobgoodiwn.me', // for base of deployed site
+  integrations: [react(), vue(), solid(), image()],
+  site: 'https://jacobgoodiwn.me',
+  // for base of deployed site
   trailingSlash: 'never',
   server: {
-    host: true, // for allowing access on local network
-  },
+    host: true // for allowing access on local network
+  }
 });
