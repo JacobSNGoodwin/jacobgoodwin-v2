3b852990016646c22c588650807812bcccd787ce
Jacob Goodwin <jacob.goodwin@gmail.com>
2022-11-13 16:10:28 -0800

    add MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,22 +9,20 @@ import image from '@astrojs/image';
 // https://astro.build/config
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    vue(),
-    solid(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-  ],
+  integrations: [react(), vue(), solid(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), mdx()],
   markdown: {
-    drafts: true,
+    drafts: true
   },
   site: 'https://jacobgoodiwn.me',
   // for base of deployed site
   trailingSlash: 'never',
   server: {
-    host: true, // for allowing access on local network
-  },
+    host: true // for allowing access on local network
+  }
 });
