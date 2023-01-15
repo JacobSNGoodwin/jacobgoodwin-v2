a916ce926b23682cca4a7e577299feda1adb77e9
Jacob Goodwin <jacob.goodwin@gmail.com>
2023-01-14 19:11:15 -0800

    add an info box for lessons

---
+++ src/utils/colors.ts
@@ -0,0 +1,8 @@
+export type SiteColor = 'blue' | 'orange' | 'green' | 'purple';
+
+export const colorMap: Record<SiteColor, string> = {
+  blue: '#002d5b',
+  orange: '#f44800',
+  green: '#00af37',
+  purple: '#45009e',
+};
