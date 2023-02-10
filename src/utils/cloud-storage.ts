8132b8b221f838c2b8e8d73e2d45eb32a8b16d8b
Jacob Goodwin <jacob.goodwin@gmail.com>
2023-02-09 19:19:06 -0800

    custom client config per env

--- src/utils/cloud-storage.ts
+++ src/utils/cloud-storage.ts
@@ -4,10 +4,23 @@ let client;
 
 export const getClient = () => {
   if (!client) {
-    console.log('creating cloud storage client');
+    const credsConfig =
+      import.meta.env.MODE === 'production'
+        ? {
+            credentials: {
+              client_email: import.meta.env.CLOUD_STORAGE_EMAIL,
+              client_secret: import.meta.env.CLOUD_STORAGE_KEY.split(
+                '\\n'
+              ).join('\n'),
+            },
+          }
+        : {};
+
+    console.log('creating cloud storage client for environment: ', credsConfig);
 
     client = new Storage({
       projectId: import.meta.env.CLOUD_STORAGE_PROJECT,
+      ...credsConfig,
     });
   }
 
